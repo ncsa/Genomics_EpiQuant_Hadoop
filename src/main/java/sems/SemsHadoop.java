9fa9cac637dc0adfb87c770ae2d4e79d5f1dcce2
Ryan Chui <Ryan@Ryans-MacBook-Air-2.local>
2017-07-15 00:20:01 -0500

    Iniital commit with old code. Testing maven dependencies and build system.

---
+++ src/main/java/sems/SemsHadoop.java
@@ -0,0 +1,8 @@
+package sems;
+
+public class SemsHadoop {
+    public static void main(String[] args) {
+        AssignJobs jobManager = new AssignJobs();
+        System.out.println("Hello World");
+    }
+}
