7d13ab9605a4b29f7a73840bdcc12c6007b287b7
Ryan Chui <Ryan@Ryans-MacBook-Air-2.local>
2017-07-15 14:29:38 -0500

    Testing doubleWritable array

--- src/main/java/managers/JobManager.java
+++ src/main/java/managers/JobManager.java
@@ -1,11 +1,15 @@
 package managers;
 
+// import assets.DoubleArrayWritable;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -15,42 +19,36 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JobManager {
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
-        private IntWritable rank = new IntWritable();
-        private Text word = new Text();
-
+    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, DoubleWritable[]>{
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             BufferedReader buff = new BufferedReader(new StringReader(value.toString()));
             
+            IntWritable rank = new IntWritable();
+            Random r = new Random();
+            rank.set(r.nextInt(4));
+
             String[] tokens;
             String line;
+            DoubleWritable[] values = new DoubleWritable[1];
             while ((line = buff.readLine()) != null) {
-                System.out.println(line);
                 tokens = line.split("\\t");
-                for (int i = 0; i < tokens.length; i++) {
-                    System.out.println(tokens[i]);
+                values = new DoubleWritable[tokens.length - 1];
+                for (int i = 1; i < tokens.length; i++) {
+                    values[i - 1].set(Double.parseDouble(tokens[i]));
                 }
             }
-            System.exit(0);
-            //   StringTokenizer itr = new StringTokenizer(value.toString());
-            //   while (itr.hasMoreTokens()) {
-            //     word.set(itr.nextToken());
-            //     context.write(word, one);
-            //   }
-            // }
+            context.write(rank, values);
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-              sum += val.get();
-            }
-            result.set(sum);
-            context.write(key, result);
+    public static class IntSumReducer extends Reducer<IntWritable,DoubleWritable[],IntWritable,DoubleWritable[]> {
+        public void reduce(IntWritable key, DoubleWritable[] values, Context context) throws IOException, InterruptedException {
+            // int sum = 0;
+            // for (IntWritable val : values) {
+            //   sum += val.get();
+            // }
+            // result.set(sum);
+            context.write(key, values);
         }
     }
 
