f4e479590c34282e4d20a59968780926ae70932c
Ryan Chui <Ryan@wirelessprv-10-192-194-67.near.illinois.edu>
2017-07-16 12:58:22 -0500

    Switching to text from array structure.

--- src/main/java/managers/JobManager.java
+++ src/main/java/managers/JobManager.java
@@ -1,17 +1,15 @@
 package managers;
 
 import java.io.IOException;
-// import java.io.BufferedReader;
-// import java.io.StringReader;
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,64 +17,39 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JobManager {
-    public static class DoubleArrayWritable extends ArrayWritable {
-        public DoubleArrayWritable() {
-            super(DoubleWritable.class);
-        }
-
-        public DoubleArrayWritable(DoubleWritable[] doubleWritables) {
-            super(DoubleWritable.class, doubleWritables);
-        }
-
-        @Override
-        public DoubleWritable[] get() {
-            return (DoubleWritable[]) super.get();
-        }
-
-        @Override
-        public String toString() {
-            DoubleWritable[] doubleWritables = get();
-            String out = "" + doubleWritables[0].get();
-            for (int i = 1; i < doubleWritables.length; i++) {
-                out += ", " + doubleWritables[i].get();
-            }
-            return out;
-        }
-    }
-
-    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, ArrayWritable>{
+    public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, Text>{
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // BufferedReader buff = new BufferedReader(new StringReader(value.toString()));
-            
+            BufferedReader buff = new BufferedReader(new StringReader(value.toString()));
             Random r = new Random();
 
-            // String[] tokens;
-            // String line;
-            DoubleWritable[] values = new DoubleWritable[5];
-            for (int i = 0; i < values.length; i++) {
-                values[i] = new DoubleWritable(r.nextDouble());
-            }
-            for (int i = 0; i < 3; i++) {
-                context.write(new IntWritable(r.nextInt(4)), new DoubleArrayWritable(values));
-            }
+            String[] tokens;
+            String line;
+			while ((line = buff.readLine()) != null) {
+                tokens = line.split("\\t");
+                String out = tokens[1];
+                for (int i = 2; i < tokens.length; i++) {
+                    out += "," + tokens[i];
+                }
+                context.write(new IntWritable(r.nextInt()), new Text(out));
+			}
         }
     }
 
-    public static class IntSumReducer extends Reducer<IntWritable, ArrayWritable, IntWritable, ArrayWritable> {
-        public void reduce(IntWritable key, ArrayWritable values, Context context) throws IOException, InterruptedException {
+    public static class IntSumReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+        public void reduce(IntWritable key, Text values, Context context) throws IOException, InterruptedException {
             context.write(key, values);
         }
     }
 
     public void run(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
+        Job job = Job.getInstance(conf, "job manager");
         job.setJarByClass(JobManager.class);
         
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(DoubleArrayWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(DoubleWritable.class);
+        job.setOutputValueClass(Text.class);
 
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
