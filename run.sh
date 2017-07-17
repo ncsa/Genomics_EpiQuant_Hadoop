e7ab86a37d9663886cd26841e21ba8e5118a0c3d
Ryan Chui <Ryan@wirelessprv-10-192-194-67.near.illinois.edu>
2017-07-17 17:09:44 -0500

    Updating with new removal

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ git pull
 mvn clean
 mvn compile
 mvn package
-hadoop fs -rm -r output
+hadoop fs -rm -r *.Split*
 hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/ /user/rchui2/pheno/pheno.txt
 hadoop fs -cat output/part-r-00000
