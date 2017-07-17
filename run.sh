5809d216383ad28cc9264c31a209c7571fb79fd4
Ryan Chui <Ryan@wirelessprv-10-192-194-67.near.illinois.edu>
2017-07-17 14:21:27 -0500

    full path to pheno.txt

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ mvn clean
 mvn compile
 mvn package
 hadoop fs -rm -r output
-hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/ /user/rchui2/pheno/
+hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/ /user/rchui2/pheno/pheno.txt
 hadoop fs -cat output/part-r-00000
