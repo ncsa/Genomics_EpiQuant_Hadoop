da2c8429ecdcd17066956cfeb05786dce6ffeefb
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-20 18:36:48 -0500

    no cating of hadoop splits

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mvn compile
 mvn package
 hadoop fs -rm -r *.Split*
 hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/ /user/rchui2/pheno/pheno.txt
-hadoop fs -cat Phenotype-*.Split-*/part-r-00000
+# hadoop fs -cat Phenotype-*.Split-*/part-r-00000
