d49e192426634befb22e81d972f6c48b25d02af9
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-20 15:16:34 -0500

    changed for multiple linear regression

--- src/main/java/utilities/ConfSet.java
+++ src/main/java/utilities/ConfSet.java
@@ -6,6 +6,22 @@ import java.util.HashSet;
 import org.apache.hadoop.conf.Configuration;
 
 public class ConfSet {
+
+    public static String getXNewString(String[] tokens) {
+        String xOut = tokens[0];
+        for (int i = 1; i < tokens.length; i++) {
+            xOut += "," + tokens[i];
+        }
+        return xOut;
+    }
+
+    public static double[][] combineX(String[] newTokens, double[][] xModel) {
+        for (int i = 0; i < xModel.length; i++) {
+            xModel[i][xModel[0].length - 1] = Double.parseDouble(newTokens[i + 1]);
+        }
+        return xModel;
+    }
+
     // Gets the y value from the context configuration.
     public static String getY(Configuration conf) {
         String[] yIn = conf.get("y").split("\\t");
@@ -23,35 +39,5 @@ public class ConfSet {
             y[i - 1] = Double.parseDouble(yStrings[i]);
         }
         return y;
-    }
-
-    // Gets the x values from the context configuration.
-    public static String[][] getX(Configuration conf) {
-        String[] xs = conf.get("x").split(":");
-        String[][] xsArray = new String[xs.length][];
-        for (int i = 0; i < xs.length; i++) {
-            xsArray[i] = xs[i].split(",");
-        }
-        return xsArray;
-    }
-
-    // Convert the x values from strings to doubles.
-    public static double[][] convertX(String[][] xStrings) {
-        double[][] xValues = new double[xStrings.length - 1][xStrings[0].length - 1];
-        for (int i = 1; i < xStrings.length; i++) {
-            for (int j = 1; j < xStrings[0].length; i++) {
-                xValues[i - 1][j - 1] = Double.parseDouble(xStrings[i][j]);
-            }
-        }
-        return xValues;
-    }
-
-    // Gets the x value labels from the context configuration.
-    public static Set<String> storeX(String[][] xStrings) {
-        Set<String> xSet = new HashSet<String>();
-        for (int i = 0; i < xStrings.length; i++) {
-            xSet.add(xStrings[i][0]);
-        }
-        return xSet;
     }
 }
