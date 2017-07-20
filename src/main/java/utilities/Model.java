23cc75b96f0a92393bafa9008ff753099d64704e
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-20 17:46:37 -0500

    Single function to set model

--- src/main/java/utilities/Model.java
+++ src/main/java/utilities/Model.java
@@ -1,9 +1,9 @@
 package utilities;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.apache.hadoop.conf.Configuration;
@@ -11,41 +11,27 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 
 public class Model {
-    // Reads in model from a file.
-    public static String getModel(String inPath) throws IOException {
-        Path path = new Path("hdfs:" + inPath);
-        FileSystem fs = FileSystem.get(new Configuration());
-        BufferedReader buff = new BufferedReader(new InputStreamReader(fs.open(path)));
-        String out = buff.readLine();
-        buff.close();
-        return out;
-    }
 
     // Writes model to a file.
     public static void setModel(String outPath, String model) throws IOException {
-        Path path = new Path(outPath);
+        Path path = new Path("hdfs:" + outPath);
         FileSystem fs = FileSystem.get(new Configuration());
+
         // Write new x to new file.
-        path = new Path("hdfs:" + outPath);
-        BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(fs.create(path)));
-        buffOut.write(model);
-        buffOut.close();           
-    }
-
-    // Update model file.
-    public static void updateModel(String inPath, String outPath) throws IOException {
-        Path path = new Path("hdfs:" + inPath);
-        FileSystem fs = FileSystem.get(new Configuration());
-        BufferedReader buffIn = new BufferedReader(new InputStreamReader(fs.open(path)));
-
-        String line = buffIn.readLine();
-        buffIn.close();
-
-        path = new Path("hdfs:" + outPath);
-        BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(fs.append(path)));
-        if (line != "") {
-            buffOut.write("\t" + line);
-        }
-        buffOut.close();
+        if (fs.exists(path)) {
+            BufferedReader buffIn = new BufferedReader(new InputStreamReader(fs.open(path)));
+            BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(fs.append(path)));
+            if (buffIn.readLine() == "") {
+                buffOut.write(model);    
+            } else {
+                buffOut.write("\t" + model);
+            }
+            buffIn.close();
+            buffOut.close();      
+        } else {
+            BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(fs.create(path)));
+            buffOut.write(model);
+            buffOut.close();   
+        }        
     }
 }
