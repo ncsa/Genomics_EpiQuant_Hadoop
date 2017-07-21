69258c93bbeeb8c019b7c10422c61977ae85d151
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-20 23:21:13 -0500

    Testing only x values

--- src/main/java/managers/JobManager.java
+++ src/main/java/managers/JobManager.java
@@ -66,7 +66,7 @@ public class JobManager {
                 if (".".equals(model)) {
                     context.write(new Text(mapKey), new Text(Double.toString(pvalue) + "\t" + ConfSet.getXNewString(tokens)));
                 } else {
-                    // context.write(new Text(mapKey), new Text(Double.toString(pvalue) + "\t" + model + "\n" + ConfSet.getXNewString(tokens)));
+                    context.write(new Text(mapKey), new Text(Double.toString(pvalue) + "\t" + model + "\n" + ConfSet.getXNewString(tokens)));
                 }
             }
         }
@@ -106,9 +106,9 @@ public class JobManager {
     public static class ModelMapper extends Mapper<Text, Text, Text, Text>{
         public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
             String[] values = value.toString().split("\\t");
-            // String[] tokens = values[2].split("[\\r\\n]+");
+            String[] tokens = values[2].split("[\\r\\n]+");
             Configuration conf = context.getConfiguration();
-            context.write(key, value);
+            context.write(key, new Text(values[2]));
         }
     }
 
