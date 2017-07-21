f6b10743f2225f4b1c06b9fd3727f6b51af83a2d
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-20 21:08:24 -0500

    No longer separating forward and backward step

--- src/main/java/sems/SEMSHadoop.java
+++ src/main/java/sems/SEMSHadoop.java
@@ -1,7 +1,6 @@
 package sems;
 
-import managers.BackwardManager;
-import managers.ForwardManager;
+import managers.JobManager;
 import utilities.Model;
 
 import java.io.BufferedReader;
@@ -17,71 +16,54 @@ import org.apache.hadoop.conf.Configuration;
 
 public class SEMSHadoop {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> fPhenoList = getPhenotypes(args);
-        ArrayList<Job> fJobList = new ArrayList<Job>();
-        ArrayList<int[]> fSplits = new ArrayList<int[]>();
-
-        ArrayList<String> bPhenoList = new ArrayList<String>();
-        ArrayList<Job> bJobList = new ArrayList<Job>();
-        ArrayList<int[]> bSplits = new ArrayList<int[]>();
+        ArrayList<String> phenoList = getPhenotypes(args);
+        ArrayList<Job> jobList = new ArrayList<Job>();
+        ArrayList<int[]> splits = new ArrayList<int[]>();
         long start = System.nanoTime();
-        String message;
+        String message, baseDir;
 
         // Submit jobs by to the job list.
-        ForwardManager fManager = new ForwardManager();
-        BackwardManager bManager = new BackwardManager();
-        for (int i = 0; i < fPhenoList.size(); i++) {
-            fSplits.add(new int[2]);
-            fSplits.get(i)[0] = i; // Phenotype number
-            fSplits.get(i)[1] = 1; // Split number
-            message = " [Task = Adding F.P-" + fSplits.get(i)[0] + ".S-" + fSplits.get(i)[1] + "]";
-            runningTime(start, fJobList.size(), false, message);
-            fJobList.add(fManager.run(args, fPhenoList.get(i), fSplits.get(i)[0], fSplits.get(i)[1]));
+        JobManager jobManager = new JobManager();
+        for (int i = 0; i < phenoList.size(); i++) {
+            splits.add(new int[2]);
+            splits.get(i)[0] = i; // Phenotype number
+            splits.get(i)[1] = 1; // Split number
+            message = " [Task = Adding F.P-" + splits.get(i)[0] + ".S-" + splits.get(i)[1] + "]";
+            runningTime(start, jobList.size(), false, message);
+            jobList.add(jobManager.run(args[1], phenoList.get(i), splits.get(i)[0], splits.get(i)[1]));
         }
 
         boolean running = true;
 
         // Track running jobs until none are left.
         while (running) {
-            runningTime(start, fJobList.size() + bJobList.size(), false, "");
-            int size = fJobList.size();
+            runningTime(start, jobList.size(), false, "");
+            int size = jobList.size();
+
             // Remove jobs if completed.
             for (int i = 0; i < size; i++) {
-                if (fJobList.get(i).isComplete()) {
-                    message = " [Task = Removing F.P-" + fSplits.get(i)[0] + ".S-" + fSplits.get(i)[1] + "]";
-                    runningTime(start, fJobList.size() + bJobList.size(), false, message);
-                    fJobList.remove(i);
+                if (jobList.get(i).isComplete()) {
+                    message = " [Task = Removing F.P-" + splits.get(i)[0] + ".S-" + splits.get(i)[1] + "]";
+                    runningTime(start, jobList.size(), false, message);
+                    jobList.remove(i);
 
-                    if (true) {
-                        String baseDir = "/user/rchui2/Phenotype-" + fSplits.get(i)[0] + ".Split-" + fSplits.get(i)[1] + "/";
-                        message = " [Task = Adding B.P-" + fSplits.get(i)[0] + ".S-" + fSplits.get(i)[1] + "]";
-                        runningTime(start, fJobList.size(), false, message);
-                        bJobList.add(bManager.run(baseDir + "part-r-00000", fPhenoList.get(i), baseDir));
-                        bSplits.add(fSplits.remove(i));
-                        bPhenoList.add(fPhenoList.remove(i));
-                    }
-                    
+                    // if (true) {
+                    //     baseDir = "/user/rchui2/Phenotype-" + splits.get(i)[0] + ".Split-" + splits.get(i)[1] + "/";
+                    //     message = " [Task = Adding B.P-" + splits.get(i)[0] + ".S-" + splits.get(i)[1] + "]";
+                    //     runningTime(start, jobList.size(), false, message);
+                    // }
+
                     i--;
                     size--;
                 }
             }
 
-            size = bJobList.size();
-            for (int i = 0; i < size; i++) {
-                if (bJobList.get(i).isComplete()) {
-                    message = " [Task = Removing B.P-" + bSplits.get(i)[0] + ".S-" + bSplits.get(i)[1] + "]";
-                    runningTime(start, bJobList.size() + bJobList.size(), false, message);
-                    bJobList.remove(i);
-                    i--;
-                    size--;
-                }
-            }
-            if (fJobList.isEmpty() && bJobList.isEmpty()) {
+            if (jobList.isEmpty()) {
                 running = false;
             }
             TimeUnit.SECONDS.sleep(2);
         }
-        runningTime(start, fJobList.size() + bJobList.size(), true, "");
+        runningTime(start, jobList.size(), true, "");
         System.exit(0);
     }
 
@@ -131,5 +113,9 @@ public class SEMSHadoop {
             System.exit(1);
         }
         return null;
+    }
+
+    public static void backStep() {
+
     }
 }
