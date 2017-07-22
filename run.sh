233d2d7f844c351d8728d044b97f896d4f7b2676
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-22 00:47:19 -0500

    Removing databuiler directory

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ mvn clean
 mvn compile
 mvn package
 hadoop fs -rm -r *.Split*
+hadoop fs -rm -r DataBuilder
 hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
 # hadoop fs -cat Phenotype-*.Split-*/part-r-00000
