7ccd4e509a3d57be43e49318a96a23987a2e4423
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-22 17:08:50 -0500

    run from hadoop not yarn

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ hadoop fs -rm -r *.Split*
 # hadoop fs -rm -r DataBuilder
 # hadoop fs -rm -r DataCleaner
 hadoop fs -rm -r .Trash/*
-yarn jar target/SEMS-Hadoop-0.1.0.jar managers.SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
+hadoop jar target/SEMS-Hadoop-0.1.0.jar managers.SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
 # hadoop fs -cat Phenotype-*.Split-*/part-r-00000
