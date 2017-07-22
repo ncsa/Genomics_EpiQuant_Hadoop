0808c3321dfe26f3bb96c596edc5ba636388e5b7
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-22 18:21:30 -0500

    exporting hadoop classpath

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ hadoop fs -rm -r DataBuilder
 hadoop fs -rm -r DataCleaner
 hadoop fs -rm -r .staging/*
 hadoop fs -rm -r .Trash/*
+export HADOOP_CLASSPATH=/ui/ncsa/rchui2/SEMS-Hadoop/target
+echo $HADOOP_CLASSPATH
 hadoop jar target/SEMS-Hadoop-0.1.0.jar managers.SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
 # hadoop fs -cat Phenotype-*.Split-*/part-r-00000
