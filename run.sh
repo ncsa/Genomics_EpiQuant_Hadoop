8338431e96dc5a81a4c2ea1677eebb3ff7e1e015
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-21 23:12:17 -0500

    snps not data

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ mvn clean
 mvn compile
 mvn package
 hadoop fs -rm -r *.Split*
-hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/data.txt /user/rchui2/pheno/pheno.txt
+hadoop jar target/SEMS-Hadoop-0.1.0.jar SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
 # hadoop fs -cat Phenotype-*.Split-*/part-r-00000
