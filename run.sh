3d178755f6bec81ec5598f02749e725e1e6cb127
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-22 14:03:11 -0500

    Don't delete during test

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ mvn clean
 mvn compile
 mvn package
 hadoop fs -rm -r *.Split*
-hadoop fs -rm -r DataBuilder
-hadoop fs -rm -r DataCleaner
+# hadoop fs -rm -r DataBuilder
+# hadoop fs -rm -r DataCleaner
 hadoop fs -rm -r .Trash/*
 hadoop jar target/SEMS-Hadoop-0.1.0.jar managers.SEMSHadoop /user/rchui2/data/snps.txt /user/rchui2/pheno/pheno.txt
 # hadoop fs -cat Phenotype-*.Split-*/part-r-00000
