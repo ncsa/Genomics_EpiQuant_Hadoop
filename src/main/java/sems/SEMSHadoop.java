5e8cc9c01021db9a3b6039872d129e89696560c1
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-24 12:01:15 -0500

    Running just 1 phenotype

--- src/main/java/sems/SEMSHadoop.java
+++ src/main/java/sems/SEMSHadoop.java
@@ -15,7 +15,6 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.Configuration;
 
-//TODO: Run a wordcount on DataBuilder
 
 public class SEMSHadoop {
     public static void main(String[] args) throws Exception {
