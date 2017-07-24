561b8cb1fa58435acc34b7b6b60d2d6d505e1b46
Ryan Chui <ryan.w.chui@gmail.com>
2017-07-24 14:59:17 -0500

    Fully commented

--- src/main/java/managers/DataCleaner.java
+++ src/main/java/managers/DataCleaner.java
@@ -17,10 +17,21 @@ import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+/**
+ * DataBuilder
+ *     Performing a word count over lines to get unique lines.
+ */
 public class DataCleaner {
     public static class TokenMapper extends Mapper<Object, Text, Text, LongWritable>{
         private final static LongWritable one = new LongWritable(1);
 
+        /**
+         * Reads in lines from mapper block and writes line as key, and 1 as value.
+         * Params:
+         *     key (Object) generic file key.
+         *     value (Text) text for this mapper block.
+         *     context (Context) object that contains job information.
+         */
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             BufferedReader buff = new BufferedReader(new StringReader(value.toString()));
             String line;
@@ -33,6 +44,14 @@ public class DataCleaner {
 
     public static class ElementReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
         private LongWritable result = new LongWritable();
+
+        /**
+         * Sums over the number of each line.
+         * Params:
+         *     key (Text) snp line string.
+         *     values (Iterable<LongWritable>) number of appearances of the line (key).
+         *     context (Context) object that contains job information.
+         */
         public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
             long sum = 0;
             for (LongWritable val : values) {
@@ -44,11 +63,26 @@ public class DataCleaner {
     }
 
     public static class ElementMapper extends Mapper<Text, LongWritable, Text, NullWritable>{
+        
+        /**
+         * Writes each unique key to a file.
+         * Params:
+         *     key (Text) unique snp line.
+         *     value (LongWritable) number of appearances for that line.
+         *     context (Context) object that contains job information.
+         */
         public void map(Text key, LongWritable value, Context context) throws IOException, InterruptedException {
             context.write(key, NullWritable.get());
         }
     }
 
+    /**
+     * Creates a new map reduce job and defines the map and reduce tasks, and defines the input and
+     * output files and file formats.
+     * Params:
+     *     inputPath (String) the path to read data from.
+     *     outputDir (String) path to write data to.
+     */
     public Job run(String inputPath, String outputDir) throws Exception {
         Configuration conf = new Configuration();
         conf.set("path", inputPath);
