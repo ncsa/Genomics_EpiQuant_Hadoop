dee43ac7a1db94be8ff911324c1bec59b5cd225c
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-19 13:21:08 -0500

    run async

--- src/main/java/managers/JobManager.java
+++ src/main/java/managers/JobManager.java
@@ -330,8 +330,8 @@ public class JobManager {
         MultipleOutputs.addNamedOutput(job, "significance", TextOutputFormat.class, Text.class, NullWritable.class);
         MultipleOutputs.addNamedOutput(job, "model", TextOutputFormat.class, Text.class, NullWritable.class);
         
-        job.waitForCompletion(false);
-        // job.submit();
+        // job.waitForCompletion(false);
+        job.submit();
         // job.monitorAndPrintJob();
         return job;
     }
